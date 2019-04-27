c17bf67f1404812741734b9e2ce7bb5468da842e
Mano M <manonmanim@visualbi.com>
2019-04-27 19:22:37 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import duplicateCount from './calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			values: ['']
+		};
+	}
+
+	onChange = (e, index) => {
+		const { values } = this.state;
+		values[index] = e.target.value;
+		this.setState({ values })
+	}
+
+	addItems = () => {
+		const { values } = this.state;
+		this.setState({
+			value: values.push('')
+		})
+	}
+
+	calculate = () => {
+		const { values } = this.state;
+		let transformedValues = values.map(value => value.split(","));
+		const duplicateValue = duplicateCount(transformedValues);
+		alert(duplicateValue);
+	}
+
+
+
+	render() {
+		const elements = this.state.values.map((value, index) =>
+			<div className='cards'>
+				<div className='card-headings'>
+					Item
+				</div>
+				<input type='text'
+					placeholder='Please enter the string with ,'
+					className='form-input'
+					value={value} key={index}
+					onChange={(e) => this.onChange(e, index)} />
+			</div>
+		);
+		return (<React.Fragment>
+			<div className="headings flex">
+				{elements}
+			</div>
+			<div className='footer'>
+				<button className='btn btn-primary' title="Add" onClick={this.addItems}>
+					<i className='icon icon--Add' />Add
+				</button>
+				<button className='btn btn-secondary' title="Calculate" onClick={this.calculate}>
+					<i className='icon icon--Calculator' /> Calcuate
+				</button>
+			</div>
+		</React.Fragment>)
+	}
 }
-
 export default App;
